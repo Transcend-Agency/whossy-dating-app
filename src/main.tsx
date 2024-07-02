a656045a5cc3da9b0e280b1c8b441093be21a199
Iamronaldosunmu <ronaldosunmu@gmail.com>
2024-07-02 14:29:02 +0100

    configure firebase

--- src/main.tsx
+++ src/main.tsx
@@ -3,6 +3,22 @@ import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
 import './index.scss'
 import { BrowserRouter } from 'react-router-dom';
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyC4KtUH2ggeSOmcQ_7WJPQpO-WeeNhpJHQ",
+  authDomain: "whossy-app.firebaseapp.com",
+  projectId: "whossy-app",
+  storageBucket: "whossy-app.appspot.com",
+  messagingSenderId: "332139466657",
+  appId: "1:332139466657:web:26e95148b069f63f2d05e0",
+  measurementId: "G-QQ1B249HVJ"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
