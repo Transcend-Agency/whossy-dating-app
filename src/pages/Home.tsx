e2924c8c0b1608e926944640c22ca5acfecb0090
Iamronaldosunmu <ronaldosunmu@gmail.com>
2024-07-10 20:45:22 +0100

    finish building the home page

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,4 +1,7 @@
 import React from 'react';
+import { motion } from 'framer-motion';
+import { Link } from 'react-router-dom';
+import Button from '../components/ui/Button';
 
 type HomeProps = {
 
@@ -6,6 +9,16 @@ type HomeProps = {
 
 const Home: React.FC<HomeProps> = () => {
 
-    return <div>Have a good coding</div>
+    return (
+        <div className='home'>
+            <motion.div initial={{ opacity: 0 }} animate={{ opacity: 1, transition: { duration: 0.35, delay: 1 } }} exit={{ opacity: 0, transition: { duration: 0.35 } }} className='home__container'>
+                <h1 className='home__title'>
+                    Heartfelt Connections Await. <br />Discover Love on Whossy.
+                </h1>
+                <Link to="/auth/login" className="button-container"><Button text='Login' /></Link>
+                <p className='home__cta'>Don't have an account? <Link to="/auth/create-account" className='home__cta__underline'>Create account</Link></p>
+            </motion.div>
+        </div>
+    )
 }
 export default Home;
