16db9ac78f2e2a04f6adb3b5c1e4be9c002a7617
Iamronaldosunmu <ronaldosunmu@gmail.com>
2024-07-12 09:39:18 +0100

    create login page

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -1,34 +1,43 @@
 import React from 'react';
-import { motion } from 'framer-motion'
-import { useNavigate } from 'react-router-dom';
+import { Link } from 'react-router-dom';
+import AuthInput from '../components/auth/AuthInput';
+import AuthModalBackButton from '../components/auth/AuthModalBackButton';
+import AuthModalHeader from '../components/auth/AuthModalHeader';
+import AuthPage from '../components/auth/AuthPage';
+import Button from '../components/ui/Button';
 
 type LoginProps = {
 
 };
 
 const Login: React.FC<LoginProps> = () => {
-    const navigate = useNavigate()
 
-    return <motion.div initial={{ opacity: 0, scale: 0.95 }} animate={{ opacity: 1, scale: 1, transition: { duration: 0.35 } }} exit={{ opacity: 0, transition: { duration: 0.3 } }} className='login'>
-        <div className='login__modal'>
-            <div onClick={() => navigate(-1)} className='login__modal__back-button'>
-                <img src="/assets/icons/back-arrow.svg" />
-            </div>
-            <div className='login__modal__header'>
-                <h1 className='login__modal__header__title'>Welcome back</h1>
-                <h2 className='login__modal__header__sub-title'>login to see who you’ve matched with ✌</h2>
-            </div>
-            <form className='login__modal__form'>
-                <input placeholder='example@gmail.com' className='login__modal__form__input' />
-                <input placeholder='password' type='password' className='login__modal__form__input' />
-                <button className='button login__modal__form__button'>Login</button>
-                <p className='login__modal__form__cta'>Forgot Password? <span className='underline'>Reset here</span></p>
+    return <AuthPage className='login'>
+        <div className='auth-page__modal'>
+            <AuthModalBackButton />
+            <AuthModalHeader title='Welcome back' subtitle='login to see who you’ve matched with ✌' />
+            <form className='auth-page__modal__form'>
+                <AuthInput placeholder='example@gmail.com' />
+                <AuthInput placeholder='password' type='password' />
+                <Button className='auth-page__modal__form__button' text='Login' />
+                <p className='auth-page__modal__form__cta'>Forgot Password? <Link to="/auth/forgot-password" className='underline'>Reset here</Link></p>
             </form>
-            <div className='login-modal__alternate-options'>
-                
+            <div className='auth-page__modal__alternate-options'>
+                <div className="auth-page__modal__alternate-options__divider">
+                    <div></div>
+                    <p>or</p>
+                    <div></div>
+                </div>
+                <div className='auth-page__modal__alternate-options__container'>
+                    <div className='auth-page__modal__alternate-options__item'><img src="/assets/icons/facebook.svg" /></div>
+                    <div className='auth-page__modal__alternate-options__item'><img src="/assets/icons/google.svg" /></div>
+                    <div className='auth-page__modal__alternate-options__item'><img src="/assets/icons/phone.svg" /></div>
+                </div>
             </div>
+            <div className="auth-page__modal__terms-and-conditions">
+                By clicking on “Login” you agree to our <Link to="/terms-and-conditions">Terms and condition</Link>. Learn how we process your data in our <Link to="/privacy-policy">Privacy Policy</Link>.</div>
 
         </div>
-    </motion.div>
+    </AuthPage>
 }
 export default Login;
