ba176e2b8685cc73c40dc90dc0b17acf1c54e0c6
Temidire <temidireowoeye@gmail.com>
2024-07-12 09:42:33 +0100

    fix

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,6 @@
 import { Route, Routes } from "react-router-dom";
 import "./index.scss";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-import Home from "./pages/Home";
-import Signup from "./components/auth/SignUpWithEmail";
-import Auth from "./pages/Auth";
 import OnboardingLayout from "./pages/OnboardingLayout";
 import RelationshipPreference from "./pages/RelationshipPreference";
 
