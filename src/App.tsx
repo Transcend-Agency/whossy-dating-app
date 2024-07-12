a2747f6c571e021163978d977220617442842f9e
Temidire <temidireowoeye@gmail.com>
2024-07-12 08:42:56 +0100

    Feat: Logic for signing up with email, facebook, google

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,20 @@
-import { Route, Routes } from 'react-router-dom'
-import './index.scss'
-import {
-  QueryClient,
-  QueryClientProvider,
-} from '@tanstack/react-query'
-import Home from './pages/Home'
+import { Route, Routes } from "react-router-dom";
+import "./index.scss";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import Home from "./pages/Home";
+import Auth from "./pages/Auth";
 
-const queryClient = new QueryClient()
+const queryClient = new QueryClient();
 
 function App() {
-
   return (
     <QueryClientProvider client={queryClient}>
-       <Routes>
-         <Route path='/' element={<Home/>} />
-       </Routes>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/signup" element={<Auth />} />
+      </Routes>
     </QueryClientProvider>
-  )
+  );
 }
 
-export default App
+export default App;
