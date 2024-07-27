52d80445dd1a256526edabb62eefdb676ede5577
Temidire <temidireowoeye@gmail.com>
2024-07-27 16:50:39 +0100

    fix: add new routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,17 @@
-import { Route, Routes, useLocation } from 'react-router-dom'
-import './index.scss'
-import {
-  QueryClient,
-  QueryClientProvider,
-} from '@tanstack/react-query'
-import AuthLayout from './pages/AuthLayout'
-import Home from './pages/Home'
+import { Route, Routes, useLocation } from "react-router-dom";
+import "./index.scss";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import AuthLayout from "./pages/AuthLayout";
+import Home from "./pages/Home";
 import { AnimatePresence } from "framer-motion";
-import Login from './pages/Login'
-import MarqueeImageSliderBackground from './components/auth/MarqueeImageSliderBackground'
-import ForgotPassword from './pages/ForgotPassword'
-import ResetPassword from './pages/ResetPassword'
-import CreateAccount from './pages/CreateAccount'
+import Login from "./pages/Login";
+import MarqueeImageSliderBackground from "./components/auth/MarqueeImageSliderBackground";
+import ForgotPassword from "./pages/ForgotPassword";
+import ResetPassword from "./pages/ResetPassword";
+import CreateAccount from "./pages/CreateAccount";
 import OnboardingLayout from "./pages/OnboardingLayout";
-import RelationshipPreference from "./pages/RelationshipPreference";
+import Onboarding from "./pages/Onboarding";
+import { Toaster } from "react-hot-toast";
 
 const queryClient = new QueryClient();
 
@@ -21,23 +19,41 @@ function App() {
   const location = useLocation();
   return (
     <QueryClientProvider client={queryClient}>
-      {location.pathname.startsWith('/auth') && <MarqueeImageSliderBackground />}
-      <AnimatePresence mode='wait'>
+      {location.pathname.startsWith("/auth") && (
+        <MarqueeImageSliderBackground />
+      )}
+      <AnimatePresence mode="wait">
         <Routes location={location} key={location.pathname}>
-          <Route path='/auth' element={<AuthLayout />}>
+          <Route path="/auth" element={<AuthLayout />}>
             <Route index element={<Home />} />
-            <Route path='login' element={<Login />} />
-            <Route path='forgot-password' element={<ForgotPassword />} />
-            <Route path='reset-password' element={<ResetPassword />} />
-            <Route path='create-account' element={<CreateAccount />} />
+            <Route path="login" element={<Login />} />
+            <Route path="forgot-password" element={<ForgotPassword />} />
+            <Route path="reset-password" element={<ResetPassword />} />
+            <Route path="create-account" element={<CreateAccount />} />
           </Route>
           <Route path="/onboarding" element={<OnboardingLayout />}>
-          <Route index element={<RelationshipPreference />} />
-        </Route>
+            <Route index element={<Onboarding />} />
+          </Route>
         </Routes>
+        <ToastContainer />
       </AnimatePresence>
     </QueryClientProvider>
   );
 }
 
 export default App;
+
+const ToastContainer: React.FC = () => {
+  return (
+    <Toaster
+      position="top-center"
+      toastOptions={{
+        style: {
+          maxWidth: "1000px",
+          fontSize: "1.6rem",
+        },
+        duration: 1500,
+      }}
+    />
+  );
+};
