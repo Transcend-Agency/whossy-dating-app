ee47848ef3e8f2f47cfffa9794dfa2b0521fe7c9
Temidire <temidireowoeye@gmail.com>
2024-07-27 16:50:11 +0100

    fix: modified to aseess firestore

--- src/firebase.ts
+++ src/firebase.ts
@@ -1,6 +1,7 @@
 import { initializeApp } from "firebase/app";
 // import { getAnalytics } from "firebase/analytics";
 import { getAuth } from "firebase/auth";
+import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
   apiKey: "AIzaSyC4KtUH2ggeSOmcQ_7WJPQpO-WeeNhpJHQ",
@@ -16,5 +17,7 @@ const firebaseConfig = {
 const app = initializeApp(firebaseConfig);
 // const analytics = getAnalytics(app);
 const auth = getAuth(app);
+// cloud firestore
+const db = getFirestore(app);
 
-export {auth}
+export {auth, db}
