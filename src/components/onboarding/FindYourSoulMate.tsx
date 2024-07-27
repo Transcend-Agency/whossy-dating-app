23fc8f727e5be1250a41ecf96c958133f46965fd
Temidire <temidireowoeye@gmail.com>
2024-07-27 16:52:58 +0100

    Feat: create section two for onboarding layout

---
+++ src/components/onboarding/FindYourSoulMate.tsx
@@ -0,0 +1,17 @@
+
+const FindYourSoulMate = () => {
+  return (
+    <section className="onboarding-page__section-two">
+      <img src="/assets/images/onboarding/soul-mate.svg" alt="" />
+      <div>
+        <h1 className="onboarding-page__header">Find your soul mate</h1>
+        <p className="onboarding-page__text">
+          Whether you swipe right or left. Your perfect match can be found on
+          Whossy. Just keep swiping
+        </p>
+      </div>
+    </section>
+  );
+};
+
+export default FindYourSoulMate;
