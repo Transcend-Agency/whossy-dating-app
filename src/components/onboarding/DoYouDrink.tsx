9d29171c2bf6c8718d464cb1e120db3ca47a528d
Temidire <temidireowoeye@gmail.com>
2024-07-27 16:52:00 +0100

    feat: create do you drink page component

---
+++ src/components/onboarding/DoYouDrink.tsx
@@ -0,0 +1,56 @@
+import OnboardingBackButton from "./OnboardingBackButton";
+import Button from "../ui/Button";
+import Skip from "./Skip";
+import RegularOptions from "./RegularOptions";
+import { useEffect, useState } from "react";
+import { OnboardingProps } from "../../types/onboarding";
+import OnboardingPage from "./OnboardingPage";
+import { useOnboardingStore } from "../../store/useStore";
+import { drinking } from "../../constants/onboarding";
+
+const DoYouDrink: React.FC<OnboardingProps> = ({ advance, goBack }) => {
+  const [active, setActive] = useState<number | null>(null);
+  const { updateOnboardingData, "onboarding-data": data } =
+    useOnboardingStore();
+  useEffect(() => {
+    if (data["drinking-preference"] !== null && data["drinking-preference"] !== undefined) {
+      setActive(data["drinking-preference"]);
+    }
+  }, []);
+
+  return (
+    <OnboardingPage>
+      <Skip advance={advance} />
+      <h1 className="onboarding-page__header">Do you drink?</h1>
+      <p className="onboarding-page__text">
+        This would be shown on your profile
+      </p>
+      <div className="onboarding-page__regular-options-container">
+        {drinking.map((item, i) => (
+          <RegularOptions
+            desc={item}
+            index={i}
+            active={active}
+            onclick={() => setActive(i)}
+          />
+        ))}
+      </div>
+      <div className="onboarding-page__section-one__buttons">
+        <OnboardingBackButton onClick={goBack} />
+        <Button
+          text="Continue"
+          onClick={() => {
+              advance();
+              updateOnboardingData({
+                "drinking-preference": active,
+              });
+          }}
+        />
+      </div>
+    </OnboardingPage>
+  );
+};
+
+export default DoYouDrink;
+
+
