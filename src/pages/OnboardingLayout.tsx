cc6a03ff4368adafb2e8389493c60a43728a435c
Temidire <temidireowoeye@gmail.com>
2024-07-27 17:00:09 +0100

    fix: create onboarding layout

--- src/pages/OnboardingLayout.tsx
+++ src/pages/OnboardingLayout.tsx
@@ -1,13 +1,16 @@
 import React from "react";
 import { Outlet } from "react-router-dom";
+import FindYourSoulMate from "../components/onboarding/FindYourSoulMate";
 
 type OnboardingLayoutProps = {};
 
 const OnboardingLayout: React.FC<OnboardingLayoutProps> = () => {
   return (
-    <div className="grid grid-cols-2 h-screen px-16 py-12 ">
-      <Outlet />
-      <div>Hello bro</div>
+    <div className="onboarding-page">
+      <section className="onboarding-page__section-one">
+        <Outlet />
+      </section>
+      <FindYourSoulMate />
     </div>
   );
 };
