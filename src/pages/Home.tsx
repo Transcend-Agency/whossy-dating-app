3383cc9857c3a7b014cb8f0c5d8b23c2ac87e635
Iamronaldosunmu <ronaldosunmu@gmail.com>
2024-07-31 09:33:46 +0100

    fix line break issue

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -11,9 +11,9 @@ const Home: React.FC<HomeProps> = () => {
 
     return (
         <div className='home'>
-            <motion.div initial={{ opacity: 0 }} animate={{ opacity: 1, transition: { duration: 0.25, delay: 1 } }} exit={{ opacity: 0, transition: { duration: 0.25 } }} className='home__container'>
+            <motion.div initial={{ opacity: 0 }} animate={{ opacity: 1, transition: { duration: 0.2, delay: 0.1 } }} exit={{ opacity: 0, transition: { duration: 0.2 } }} className='home__container'>
                 <h1 className='home__title'>
-                    Heartfelt Connections Await. <br />Discover Love on Whossy.
+                    Heartfelt Connections Await. <br className='home__line-break'/>Discover Love on Whossy.
                 </h1>
                 <Link to="/auth/login" className="button-container"><Button text='Login' /></Link>
                 <p className='home__cta'>Don't have an account? <Link to="/auth/create-account" className='home__cta__underline'>Create account</Link></p>
