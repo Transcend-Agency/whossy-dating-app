567400e0599367b37d55b3a9e7476e97bb91c1ae
Iamronaldosunmu <ronaldosunmu@gmail.com>
2024-07-31 09:17:21 +0100

    create auth page component

--- src/components/auth/AuthPage.tsx
+++ src/components/auth/AuthPage.tsx
@@ -9,7 +9,7 @@ type AuthPageProps = {
 
 const AuthPage: React.FC<AuthPageProps> = ({ children, className, key }) => {
 
-    return <motion.div key={key} initial={{ opacity: 0, scale: 0.95 }} animate={{ opacity: 1, scale: 1, transition: { duration: 0.25 } }} exit={{ opacity: 0, scale: 0.95, transition: { duration: 0.25 } }} className={`auth-page ${className}`} >
+    return <motion.div key={key} initial={{ opacity: 0, scale: 0.98 }} animate={{ opacity: 1, scale: 1, transition: { duration: 0.2 } }} exit={{ opacity: 0, scale: 0.98, transition: { duration: 0.2 } }} className={`auth-page ${className}`} >
         {children}
     </motion.div>
 }
