50e21c40f9979b180e6b8efe0eb46e1a978d7229
Iamronaldosunmu <ronaldosunmu@gmail.com>
2024-07-31 09:35:37 +0100

    create phone number sign in zustand store

---
+++ src/store/PhoneNumberSignIn.tsx
@@ -0,0 +1,22 @@
+import { ConfirmationResult } from 'firebase/auth';
+import create from 'zustand';
+
+type PhoneNumberStore = {
+    phone_number: string;
+    verification_id: string;
+    confirmationResult: any;
+    setPhoneNumber: (phone_number: string) => void;
+    setVerificationId: (verification_id: string) => void;
+    setConfirmationResult: (confirmationResult: any) => void;
+};
+
+const usePhoneNumberStore = create<PhoneNumberStore>((set, get) => ({
+    phone_number: '',
+    verification_id: '',
+    confirmationResult: {},
+    setConfirmationResult: (confirmationResult: any) => set(() => ({confirmationResult})),
+    setPhoneNumber: (phone_number: string) => set(() => ({ phone_number })),
+    setVerificationId: (verification_id: string) => set(() => ({verification_id}))
+}));
+
+export default usePhoneNumberStore;
