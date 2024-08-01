47908d908d6824da627075c9cccfd2cdb6f7f8d7
Temidire <temidireowoeye@gmail.com>
2024-08-01 21:32:59 +0100

    fix: image size

--- src/components/onboarding/FindYourSoulMate.tsx
+++ src/components/onboarding/FindYourSoulMate.tsx
@@ -1,8 +1,11 @@
-
 const FindYourSoulMate = () => {
   return (
-    <section className="onboarding-page__section-two">
-      <img src="/assets/images/onboarding/soul-mate.svg" alt="" />
+    <section className="onboarding-page__section-two flex flex-col justify-center">
+      <img
+        src="/assets/images/onboarding/soul-mate.svg"
+        className="w-full"
+        alt=""
+      />
       <div>
         <h1 className="onboarding-page__header">Find your soul mate</h1>
         <p className="onboarding-page__text">
