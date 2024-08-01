1f686d127f61e4ee9e5ea5d3ddf7950acca2a82d
Temidire <temidireowoeye@gmail.com>
2024-08-01 21:35:40 +0100

    feat: add pets component

--- src/components/onboarding/PetsAndWorkout.tsx
+++ src/components/onboarding/PetsAndWorkout.tsx
@@ -5,30 +5,40 @@ import Skip from "./Skip";
 import RegularOptions from "./RegularOptions";
 import { OnboardingProps } from "../../types/onboarding";
 import OnboardingPage from "./OnboardingPage";
-import { useOnboardingStore } from "../../store/useStore";
+import { useOnboardingStore } from "../../store/onboarding/useStore";
 import { workout } from "../../constants/onboarding";
+import Pets from "./Pets";
 // import OnboardingBackButton from "../components/onboarding/OnboardingBackButton";
 
 const PetsAndWorkout: React.FC<OnboardingProps> = ({ advance, goBack }) => {
   const [active, setActive] = useState<number | null>(null);
-  const { updateOnboardingData, "onboarding-data":data } = useOnboardingStore();
+  const { updateOnboardingData, "onboarding-data": data } =
+    useOnboardingStore();
   useEffect(() => {
-    if (data["workout-preference"] !== null && data["workout-preference"] !== undefined) {
-      setActive(data["workout-preference"])
+    if (
+      data["workout-preference"] !== null &&
+      data["workout-preference"] !== undefined
+    ) {
+      setActive(data["workout-preference"]);
     }
- }, [])
+  }, []);
 
   return (
     <OnboardingPage>
       <Skip advance={advance} />
       <h1 className="onboarding-page__header">Do you own any pets?</h1>
       <p className="onboarding-page__text">
-        This allows us to suggest the right people for you. You can always
-        change this later.
+        This will be shown on your profile
       </p>
+      <Pets />
+      <hr style={{ marginTop: "4rem" }} />
       <div className="onboarding-page__regular-options-container">
+        <h1 style={{ color: "black", fontSize: "1.8rem", fontWeight: 500 }}>
+          Do you work out?
+        </h1>
         {workout.map((item, i) => (
           <RegularOptions
+            key={i}
             desc={item}
             index={i}
             active={active}
@@ -41,10 +51,10 @@ const PetsAndWorkout: React.FC<OnboardingProps> = ({ advance, goBack }) => {
         <Button
           text="Continue"
           onClick={() => {
-              advance();
-              updateOnboardingData({
-                "workout-preference": active,
-              });
+            advance();
+            updateOnboardingData({
+              "workout-preference": active,
+            });
           }}
         />
       </div>
@@ -53,4 +63,3 @@ const PetsAndWorkout: React.FC<OnboardingProps> = ({ advance, goBack }) => {
 };
 
 export default PetsAndWorkout;
-
