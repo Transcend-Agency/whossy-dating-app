d1257bfa861fe53c0c378646feaf597067ccae6e
Temidire <temidireowoeye@gmail.com>
2024-09-06 15:39:49 +0100

    fix: change name from privateroute to protectedroute

--- src/pages/ProtectedRoute.tsx
+++ src/pages/ProtectedRoute.tsx
@@ -2,14 +2,14 @@ import { auth } from "@/firebase";
 import React from "react";
 import { Navigate } from "react-router-dom";
 
-interface PrivateRouteProps {
+interface ProtectedRouteProps {
     children: React.ReactNode;
   };
 
-const PrivateRoute: React.FC<PrivateRouteProps> = ({ children }) => {
+const ProtectedRoute: React.FC<ProtectedRouteProps> = ({ children }) => {
   const user = auth.currentUser;
 
   return user ? children : <Navigate to="/auth" />;
 }
 
-export default PrivateRoute;
+export default ProtectedRoute;
