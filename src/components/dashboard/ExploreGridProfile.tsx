4c9dc66f8a3d3e9b596bd28537e2b1c852128f86
Ronald Dosunmu <ronaldosunmu@gmail.com>
2024-09-06 19:42:52 +0100

    add profile click

--- src/components/dashboard/ExploreGridProfile.tsx
+++ src/components/dashboard/ExploreGridProfile.tsx
@@ -1,8 +1,22 @@
-const ExploreGridProfile = () => {
+interface ExploreGridProfileProps {
+    profile_image: string
+    distance: string;
+    first_name: string;
+    age: number;
+    onProfileClick: () => void
+}
+
+const ExploreGridProfile: React.FC<ExploreGridProfileProps> = ({
+    profile_image,
+    distance,
+    first_name,
+    age,
+    onProfileClick
+}) => {
     return (
-        <div className='explore-grid__profile'>
+        <div onClick={onProfileClick} className='explore-grid__profile'>
             <figure className='explore-grid__profile-picture'>
-                <img className='explore-grid__profile-picture-image' src="/assets/images/dashboard/sample-person.png" />
+                <img className='explore-grid__profile-picture-image' src={profile_image} />
                 <img className='explore-grid__profile-picture-gradient' src="/assets/images/dashboard/bottom-gradient.png" />
             </figure>
             <div className='explore-grid__profile-info'>
@@ -11,11 +25,11 @@ const ExploreGridProfile = () => {
                         <img src="/assets/icons/leaf.svg" />
                         New
                     </figure>
-                    <span>~ 20 mi away</span>
+                    <span>~ {distance}</span>
                 </div>
                 <div className='explore-grid__profile-info-bottom'>
-                    <span className='explore-grid__profile-name'>Jessica,</span>
-                    <span className='explore-grid__profile-age'>21</span>
+                    <span className='explore-grid__profile-name'>{first_name},</span>
+                    <span className='explore-grid__profile-age'>{age}</span>
                     <img src='/assets/icons/verified.svg' />
                 </div>
             </div>
