45b14d2b2b14b8c665c44dc21654b1d31c25eb3c
Temidire <temidireowoeye@gmail.com>
2024-09-06 15:06:46 +0100

    fix: active icon based on route

--- src/components/dashboard/BottomNav.tsx
+++ src/components/dashboard/BottomNav.tsx
@@ -1,4 +1,3 @@
-import { useState } from "react";
 import {
   chat,
   globalSearch,
@@ -9,25 +8,24 @@ import {
   profileTop,
 } from "../../assets/icons";
 import Icon from "../ui/Icon";
-import { useNavigate } from "react-router-dom";
+import { useLocation, useNavigate } from "react-router-dom";
 
 const BottomNav = () => {
-  const [active, setActive] = useState<string>("match");
   const navigate = useNavigate();
+  const {pathname} = useLocation();
   return (
-    <div className="sticky bottom-0 w-full bg-white text-white flex">
+    <div className="fixed bottom-0 w-full bg-white text-white flex">
       {icons.map((icon) => (
         <div
           className={` w-full py-[2rem] flex justify-center`}
-          onClick={() => setActive(icon.name)}
         >
           <Icon
-            color={active === icon.name ? "#F2243E" : "#8A8A8E"}
+            color={pathname === icon.route ? "#F2243E" : "#8A8A8E"}
             id={icon.name}
             src={icon.src}
             src2={icon.src2}
             className="cursor-pointer"
-            onClick={() => navigate("/dashboard/" + icon.route)}
+            onClick={() => navigate(icon.route)}
           />
         </div>
       ))}
@@ -38,9 +36,14 @@ const BottomNav = () => {
 export default BottomNav;
 
 const icons = [
-  { name: "match", src: match, route: "" },
-  { name: "globalSearch", src: globalSearch, route: "globalSearch" },
-  { name: "heart", src: heartTop, src2: heartBottom, route: "heart" },
-  { name: "chat", src: chat, route: "chat" },
-  { name: "profile", src: profileTop, src2: profileBottom, route: "profile" },
+  { name: "match", src: match, route: "/dashboard/matches" },
+  { name: "globalSearch", src: globalSearch, route: "/dashboard/globalSearch" },
+  { name: "heart", src: heartTop, src2: heartBottom, route: "/dashboard/heart" },
+  { name: "chat", src: chat, route: "/dashboard/chat" },
+  {
+    name: "profile",
+    src: profileTop,
+    src2: profileBottom,
+    route: "/dashboard/user-profile",
+  },
 ];
