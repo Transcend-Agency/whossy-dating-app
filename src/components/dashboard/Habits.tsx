5b3e40758b43753f7296f70cf66533e9c017b842
Temidire <temidireowoeye@gmail.com>
2024-09-06 15:08:59 +0100

    fix

--- src/components/dashboard/Habits.tsx
+++ src/components/dashboard/Habits.tsx
@@ -8,14 +8,15 @@ import HabitSearch from "./HabitSearch";
 
 interface HabitsProps {
   path: string;
+  closePage?: () => void;
 }
 
-const Habits: React.FC<HabitsProps> = ({ path }) => {
+const Habits: React.FC<HabitsProps> = ({ path, closePage }) => {
   const [initData, setInitData] = useState<string[]>([]);
   const [mutatedData, setMutatedData] = useState<string[]>([]);
 
   const fetchInterests = async () => {
-    const userProfile = (await useGetUserProfile(path)) as
+    const userProfile = (await useGetUserProfile(path as 'filters' | 'users' | 'preferences')) as
       | UserPrefences
       | UserFilters;
 
@@ -33,9 +34,9 @@ const Habits: React.FC<HabitsProps> = ({ path }) => {
   }, [path]);
 
   const arraysAreEqual = (arr1: string[], arr2: string[]) => {
-    if (arr1.length !== arr2.length) return false;
-    for (let i = 0; i < arr1.length; i++) {
-      if (!arr2.includes(arr1[i])) return false;
+    if (arr1?.length !== arr2?.length) return false;
+    for (let i = 0; i < arr1?.length; i++) {
+      if (!arr2?.includes(arr1[i])) return false;
     }
     return true;
   };
@@ -47,7 +48,7 @@ const Habits: React.FC<HabitsProps> = ({ path }) => {
   }, [mutatedData]);
 
   const handleUpdate = () => {
-    useUpdateUserProfile(path, fetchInterests, {
+    useUpdateUserProfile(path as 'filters' | 'users' | 'preferences', fetchInterests, {
       interests: mutatedData,
     });
   };
@@ -64,11 +65,22 @@ const Habits: React.FC<HabitsProps> = ({ path }) => {
 
   return (
     <>
-      <ProfileHeader
+      {/* <ProfileHeader
         title="Interests"
         changed={hasChanged}
         save={handleUpdate}
-      />
+      /> */}
+       <header className="profile__header">
+          <div className="profile__header__name">
+          <img src="/assets/icons/left-arrow-black.svg" alt="Back Arrow" />
+          <h1 onClick={closePage}>Interests</h1>
+          </div>
+          {hasChanged && (
+          <button className="profile__header__save" onClick={handleUpdate}>
+          Save
+          </button>
+          )}
+      </header>
       <main className="px-6">
         <HabitSearch
           initData={mutatedData}
@@ -83,13 +95,13 @@ const Habits: React.FC<HabitsProps> = ({ path }) => {
                   key={i}
                   className="text-[1.6rem] inline-block mr-[1rem] w-fit rounded-md px-[0.6rem] py-[0.8rem] cursor-pointer transition-all duration-150"
                   style={{
-                    border: mutatedData.includes(option)
+                    border: mutatedData?.includes(option)
                       ? "1px solid black"
                       : "1px solid #8A8A8E",
-                    backgroundColor: mutatedData.includes(option)
+                    backgroundColor: mutatedData?.includes(option)
                       ? "black"
                       : "transparent",
-                    color: mutatedData.includes(option) ? "white" : "black",
+                    color: mutatedData?.includes(option) ? "white" : "black",
                   }}
                   onClick={() => handleClick(option)}
                 >
