9e85ba5fbdd90322ecf2eb0c2d77bd94e19b77cf
Temidire <temidireowoeye@gmail.com>
2024-10-01 08:42:28 +0100

    fix: add skeleton while chats are loading

--- src/components/dashboard/ChatListItem.tsx
+++ src/components/dashboard/ChatListItem.tsx
@@ -9,15 +9,15 @@ interface ChatListItemProps {
     openChat?: () => void;
 }
 
-export const ChatListItem: React.FC<ChatListItemProps> = ({profileImage, contactName, message, messageStatus, openChat}) => {
+export const ChatListItem: React.FC<ChatListItemProps> = ({profileImage, contactName, message, messageStatus, openChat, onlineStatus}) => {
   return (
     <div className='flex justify-between cursor-pointer hover:bg-[#f9f8f8] px-[1.6rem] pb-[0.6rem] pt-[1.4rem] transition-all duration-300 ease-in-out transform hover:scale-[1.02]' style={{borderBottom: '1px solid #F6F6F6'}} onClick={openChat}>
         <div className='flex gap-x-[0.8rem]'> 
             <div className='relative'>
                 {profileImage ? <img className='size-[5.6rem] object-cover rounded-full' src={profileImage} alt="profile picture" /> : <div className='bg-[#D3D3D3] size-[5.6rem] rounded-full text-[1.8rem] font-semibold flex justify-center items-center'>{contactName.charAt(0)}</div>}
-                <div className='bg-white p-[0.2rem] absolute bottom-0 right-0 rounded-full'>
+               { onlineStatus && <div className='bg-white p-[0.2rem] absolute bottom-0 right-0 rounded-full'>
                     <div className='bg-[#0CB25A] size-[1.4rem] rounded-full'/>
-                </div>
+                </div>}
             </div>
             <div>
                 <p className='text-[1.8rem] leading-[2.16rem]'>{contactName}</p>
