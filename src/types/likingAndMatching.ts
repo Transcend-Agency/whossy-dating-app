f19607c9546c81c8a2f00d72ded001c327f5d331
Ronald Dosunmu <ronaldosunmu@gmail.com>
2024-10-04 23:17:22 +0100

    work on like and match feature

--- src/types/likingAndMatching.ts
+++ src/types/likingAndMatching.ts
@@ -1,3 +1,5 @@
+import { User } from "./user";
+
 export interface Like {
     liker_id: string;
     liked_id: string;
@@ -8,4 +10,8 @@ export interface Match {
     user1_id: string,
     user2_id: string,
     match_timestamp: string,
+}
+
+export interface PopulatedLikeData extends Like {
+    liker: User
 }
