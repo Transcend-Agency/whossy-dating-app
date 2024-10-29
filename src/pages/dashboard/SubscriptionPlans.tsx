18d70b23fdeedcfaf90db0dd84a5595256ca0517
Wande Teniola <wandecreates@gmail.com>
2024-10-29 21:00:55 +0100

    fix: header subscription issues

--- src/pages/dashboard/SubscriptionPlans.tsx
+++ src/pages/dashboard/SubscriptionPlans.tsx
@@ -98,12 +98,7 @@ const SubscriptionPlans: React.FC<SubscriptionPlansProps> = ({ activePage, close
                         {/* <button className="settings-page__title__save-button">Save</button> */}
                     </div>
                     <section className="py-[0.8rem] flex relative  h-[12rem]" >
-                        <div className="w-full px-[2.4rem]">
-                               <div className=" min-w-full px-[1.2rem] py-[1.6rem] text-[#AAAAAA] bg-gradient-to-r from-[#E3E3E3] to-white " style={{border: '1.5px solid #AAAAAA', borderRadius: '1.2rem'}}>
-                                    <h1 className="text-[2.4rem] font-bold ">Whossy Free Plan</h1>
-                                    <p className="flex gap-[0.4rem]"><span className="text-[1.6rem] font-semibold self-center">$</span><span className="text-[3.2rem] font-medium self-end">0</span><span className="text-[1.6rem] font-bold self-end">/month</span></p>
-                                </div>
-                        </div>
+                        
                        <PremiumPlansHeader plan={plan === 'premium'}/>
                     </section>
                     <div className="flex justify-center gap-x-[0.8rem] relative">
