398f9f0b1a4acc43f82caefc068b1bd05dbc6dc1
devhnry <devthenry@gmail.com>
2024-10-30 23:50:27 +0100

    feat: implemented block functionality, blocked list exists on user-profile

--- src/hooks/useUser.ts
+++ src/hooks/useUser.ts
@@ -1,12 +1,11 @@
 import { doc, getDoc, setDoc, updateDoc } from "firebase/firestore";
 import { db } from "@/firebase";
-import { AdvancedSearchPreferences, User, UserFilters, UserPrefences, UserProfile } from "@/types/user";
+import { AdvancedSearchPreferences, User, UserFilters, UserProfile } from "@/types/user";
 import toast from "react-hot-toast";
 
 type CollectionName = "users" | "preferences" | "filters";
 
 const collection_one = "users";
-const collection_two = "preferences";
 const collection_three = "filters";
 
 const getUserProfile = async (
@@ -16,14 +15,9 @@ const getUserProfile = async (
   const docSnap = await getDoc(docRef);
 
   if (docSnap.exists()) {
-    // console.log("Document data:", docSnap.data() as User);
-
-    // check the path i.e the collection name that was pased
     if (path === collection_one) return docSnap.data() as User;
-    if (path === collection_two) return docSnap.data() as UserPrefences;
     if (path === collection_three) return docSnap.data() as UserFilters;
   } else {
-    // docSnap.data() will be undefined in this case
     console.log("No such document!");
   }
 };
@@ -57,9 +51,6 @@ const getAdvancedSearchPreferences = async (uid: string) => {
   const docSnap = await getDoc(docRef);
 
   if (docSnap.exists()) {
-    // console.log("Document data:", docSnap.data() as User);
-
-    // check the path i.e the collection name that was pased
     return docSnap.data() as AdvancedSearchPreferences;
   } else {
     // docSnap.data() will be undefined in this case
@@ -71,33 +62,5 @@ const getAdvancedSearchPreferences = async (uid: string) => {
     } as AdvancedSearchPreferences);
   }
 }
-// const uploadPhotosToGetUrl = (file: File, i: number, addPhotos: (url: string) => void) => {
-//   if (!file) return;
-//   const storage = getStorage();
-//   const storageRef = ref(storage, `tests/${uid as string}/profile_pictures/image_${i}`);
-//   uploadBytes(storageRef, file)
-//     .then(() => {
-//       // toast.success("Image has been uploaded successfully 🚀");
-//       console.log("File was uploaded was successfully!");
-//       getDownloadURL(storageRef)
-//         .then((url) => {
-//           addPhotos(url);
-//         })
-//         .catch((error) => console.log(error));
-//     })
-//     .catch((err) => console.log(err));
-// };
-
-// const uploadPhotosToFirestore = async (photo: string[]) => {
-//   console.log("Loading...");
-//   try {
-//     await setDoc(doc(db, collection_two, uid as string), {photo});
-//     // toast.success("Account has been created successfully 🚀");
-//     // setOpenModal(false);
-//     // navigate('/dashboard');
-//   } catch (err) {
-//     console.log(err);
-//   }
-// };
 
 export { getUserProfile, updateUserProfile, updateAdvancedSearchPreferences, getAdvancedSearchPreferences };
