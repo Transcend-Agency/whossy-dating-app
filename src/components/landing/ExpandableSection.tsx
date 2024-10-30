0e2c99c192f0afa98feacf2c254c2e09130de68f
Wande Teniola <wandecreates@gmail.com>
2024-10-30 18:27:25 +0100

    fix: faq and contact page issues

--- src/components/landing/ExpandableSection.tsx
+++ src/components/landing/ExpandableSection.tsx
@@ -1,42 +1,41 @@
 import React, { useState } from 'react';
-import { Add } from "iconsax-react"
+import { Add, Minus } from "iconsax-react";
 
 interface SectionProps {
   placeholder: string;
+  answer: string;
 }
 
-const ExpandableSection: React.FC<SectionProps> = ({ placeholder }) => {
+const ExpandableSection: React.FC<SectionProps> = ({ placeholder, answer }) => {
   const [isExpanded, setIsExpanded] = useState(false);
-  const [message, setMessage] = useState('');
 
   const toggleExpand = () => {
-    setIsExpanded(true); 
+    setIsExpanded(!isExpanded);
   };
 
   return (
     <div className="flex justify-center items-center">
-  <div className="my-[1.6rem] w-full max-w-[80rem]">
-    <div className="flex justify-between items-center bg-lightgray px-12 h-[5.1rem]">
-      <p className="text-gray">{!isExpanded ? placeholder : ''}</p>
-      <button onClick={toggleExpand} className="text-gray">
-        <Add size="18" color="#8A8A8E" />
-      </button>
-    </div>
+      <div className="my-[1.6rem] w-full max-w-[80rem]">
+        <div className="flex justify-between items-center bg-lightgray px-12 h-[5.1rem]">
+          <p className="text-gray">{placeholder}</p>
+          <button onClick={toggleExpand} className="text-gray">
+            {isExpanded ? <Minus size="18" color="#8A8A8E" /> : <Add size="18" color="#8A8A8E" />}
+          </button>
+        </div>
 
-    {isExpanded && (
-      <div className="mt-2">
-        <input
-          type="text"
-          value={message}
-          onChange={(e) => setMessage(e.target.value)}
-          className="w-full border px-4 py-2 rounded-lg"
-          placeholder="Type your message"
-        />
+        
+        <div
+          className={`overflow-hidden transition-all duration-500 ease-in-out ${
+            isExpanded ? 'max-h-96 opacity-100' : 'max-h-0 opacity-0'
+          }`}
+        >
+          <div className="mt-5 rounded-full p-7" style={{ border: "1px solid #FF5C00" }}>
+            {answer}
+          </div>
+        </div>
       </div>
-    )}
-  </div>
-</div>
+    </div>
   );
 };
 
-export default ExpandableSection
+export default ExpandableSection;
