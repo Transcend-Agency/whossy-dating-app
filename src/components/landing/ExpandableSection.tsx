6ca508bd0b2cef827573a32d8eb8d0964a04f474
Wande Teniola <wandecreates@gmail.com>
2024-10-31 16:05:02 +0100

    fix: landing page styling issues

--- src/components/landing/ExpandableSection.tsx
+++ src/components/landing/ExpandableSection.tsx
@@ -4,9 +4,12 @@ import { Add, Minus } from "iconsax-react";
 interface SectionProps {
   placeholder: string;
   answer: string;
+  index: number
+  isOpen: boolean
+  toggleSection: (index: number) => void
 }
 
-const ExpandableSection: React.FC<SectionProps> = ({ placeholder, answer }) => {
+const ExpandableSection: React.FC<SectionProps> = ({ placeholder, answer, index, isOpen, toggleSection }) => {
   const [isExpanded, setIsExpanded] = useState(false);
 
   const toggleExpand = () => {
@@ -16,20 +19,17 @@ const ExpandableSection: React.FC<SectionProps> = ({ placeholder, answer }) => {
   return (
     <div className="flex justify-center items-center">
       <div className="my-[1.6rem] w-full max-w-[80rem]">
-        <div className="flex justify-between items-center bg-lightgray px-12 h-[5.1rem]">
-          <p className="text-gray">{placeholder}</p>
+        <div className="flex justify-between items-center bg-lightgray px-12 h-[5.1rem]" onClick={() => toggleSection(index)}>
+          <p>{placeholder}</p>
+
           <button onClick={toggleExpand} className="text-gray">
-            {isExpanded ? <Minus size="18" color="#8A8A8E" /> : <Add size="18" color="#8A8A8E" />}
+            {isOpen ? <Minus size="18" color="#8A8A8E" /> : <Add size="18" color="#8A8A8E" />}
           </button>
         </div>
 
         
-        <div
-          className={`overflow-hidden transition-all duration-500 ease-in-out ${
-            isExpanded ? 'max-h-96 opacity-100' : 'max-h-0 opacity-0'
-          }`}
-        >
-          <div className="mt-5 rounded-full p-7" style={{ border: "1px solid #FF5C00" }}>
+        <div className={`transition-all duration-300 ease-in-out ${isOpen ? 'max-h-screen' : 'max-h-0 overflow-hidden'}`}>
+          <div className="text-gray py-5 px-12 bg-lightgray text-[1.4rem] lg:leading-[2.5rem]">
             {answer}
           </div>
         </div>
