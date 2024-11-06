06a9189b6a607cd1b8d0f313fca759c27a3478f4
Temidire <temidireowoeye@gmail.com>
2024-11-06 19:07:07 +0100

    fix: message being read issue

--- src/pages/Chat.tsx
+++ src/pages/Chat.tsx
@@ -12,6 +12,8 @@ import { useChatIdStore } from '@/store/ChatStore';
 import { ChatListItem, ChatListItemLoading } from '@/components/dashboard/ChatListItem';
 import { getUserProfile } from '@/hooks/useUser';
 
+// type UserProfileProps = {};
+
 interface ChatDataWithUserData extends Chat {
   user: User;
 }
@@ -35,12 +37,13 @@ const ChatPage = () => {
   // const [allChats, setAllChats] = useState<string[]>([]);
   const [allChats, setAllChats] = useState<ChatDataWithUserData[]>([]);
 
+
   const fetchUserData = async (userId: string) => {
     const userDocRef = doc(db, "users", userId);
     const userDocSnap = await getDoc(userDocRef);
-    if (userDocSnap.exists()) {
+    if (userDocSnap.exists()) { 
       return userDocSnap.data();
-    }
+    } 
     else {  console.log(`No such user document for user_id: ${userId}`); return null;}
   };
 
@@ -157,9 +160,7 @@ const ChatPage = () => {
                   {!isLoadingChats ? allChats.length === 0 ? <div className='text-[1.6rem] font-medium mb-4 px-[1.6rem] flex flex-col justify-center items-center h-full text-[#D3D3D3]'><p>No messages yet, go to the explore page to start chatting</p>. <button className='bg-[#F2243E] text-white py-3 px-6 rounded-lg active:scale-[0.95] transition ease-in-out duration-300 hover:scale-[1.02]' onClick={(e) => {e.preventDefault(); navigate('/dashboard/swipe-and-match');}}>Explore</button></div> : 
                   allChats.map((chat, i) => (
                     chat ? <><ChatListItem key={i} userData={userData as User} messageStatus={chat.status === "sent" ? chat.last_sender_id === auth?.uid ? false : true : false} onlineStatus={chat.user.status?.online} contactName={chat.user.first_name as string} message={chat.last_message} profileImage={ chat.user.photos && chat.user.photos[0] } 
-                      openChat={() => {
-                        setActivePage('selected-chat'); navigate(`/dashboard/chat?recipient-user-id=${chat.user.uid}`); setChatId(chat.participants[0] + '_' + chat.participants[1]);
-                        // setChatParticipants(chat.participants[0] + '_' + chat.participants[1]);
+                    openChat={() => {setActivePage('selected-chat'); navigate(`/dashboard/chat?recipient-user-id=${chat.user.uid}`); setChatId(chat.participants[0] + '_' + chat.participants[1]);
                     }}
                      />
                       </>
@@ -168,9 +169,7 @@ const ChatPage = () => {
                    : Array.from({ length: 7 }).map(() => <ChatListItemLoading />)}
                 </section>
             </motion.div>
-            <SelectedChat activePage={activePage}
-                          closePage={() => {setActivePage('chats');navigate('/dashboard/chat')}}
-                          updateChatId={updateChatId} userData={userData as User}/>
+            <SelectedChat activePage={activePage} closePage={() => {setActivePage('chats'); navigate('/dashboard/chat')}} updateChatId={updateChatId} userData={userData as User}/>
 
         </DashboardPageContainer>
     </>)
