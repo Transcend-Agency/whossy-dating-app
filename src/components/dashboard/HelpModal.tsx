a34f3845e656c94af1ab9b3e98d4074b37ec660d
devhnry <devwhenry@gmail.com>
2024-11-07 15:11:03 +0100

    fix: ui fixes

--- src/components/dashboard/HelpModal.tsx
+++ src/components/dashboard/HelpModal.tsx
@@ -1,5 +1,9 @@
 import { AnimatePresence, motion } from 'framer-motion'
-import {FC} from "react";
+import {FC, useState} from "react";
+import {useAuthStore} from "@/store/UserId.tsx";
+import {collection, addDoc } from "firebase/firestore";
+import {db} from "@/firebase";
+import toast from "react-hot-toast";
 
 interface HelpModalProps {
   show: boolean
@@ -13,6 +17,36 @@ const modalVariants = {
 };
 
 const HelpModal: FC<HelpModalProps> = ({ show, onCloseModal }) => {
+
+  const { user } = useAuthStore()
+  const [loading, setLoading] = useState<boolean>(false)
+  const [message, setMessage] = useState<string>("")
+
+  const requestHelp = async () => {
+    if (!message.trim()) return; // Avoid sending empty messages
+
+    setLoading(true);
+    try {
+      await addDoc(collection(db, "helpRequests") , {
+        userId: user?.uid,
+        firstName: user?.first_name,
+        lastName: user?.last_name,
+        message: message,
+        createdAt: new Date().toISOString(),
+      });
+
+      setMessage("");
+      onCloseModal();
+      toast.success("Request Received Successfully. We'll get back to you soon");
+    } catch (error) {
+      toast.error("Error sending help request");
+    } finally {
+      setLoading(false);
+    }
+  };
+
+
+
   return (
     <AnimatePresence mode='wait'>
       {show &&
@@ -20,12 +54,19 @@ const HelpModal: FC<HelpModalProps> = ({ show, onCloseModal }) => {
           <motion.div initial={{ opacity: 0 }} animate={{ opacity: 1 }} exit={{ opacity: 0 }} transition={{ duration: 0.3 }} className="bg-white text opacity-100 z-[9999] py-[2rem] px-[4rem] space-y-[3rem] rounded-[2rem]">
             <div>
                 <h1 className='text-3xl font-bold flex justify-center mb-[2rem]'>Help & Support</h1>
-                <textarea name="help-box" id="help-box" placeholder="Enter your problem" className="bg-white border border-gray focus:outline-none rounded-lg px-4 py-4 w-[369px] h-[169px] placeholder:text-2xl text-2xl"></textarea>
+                <textarea name="help-box"
+                           id="help-box"
+                           placeholder="Enter your problem"
+                           className="bg-white border border-gray text-[2.1rem] focus:outline-none rounded-lg px-4 py-4 w-[369px] h-[169px] placeholder:text-[2.1rem]"
+                           value={message}
+                           onChange={(e) => setMessage(e.target.value)}></textarea>
             </div>
 
             <div className="flex flex-row gap-x-4"> 
                 <button className="bg-[#F6F6F6] py-[1.3rem] w-full text-[1.8rem] font-bold text-center rounded-lg hover:text-white hover:bg-[#F2243E] transition-all duration-300 cursor-pointer" onClick={onCloseModal}>Cancel</button>
-                <button className="bg-[#F6F6F6] py-[1.3rem] w-full text-[1.8rem] font-bold text-center rounded-lg hover:text-white hover:bg-[#F2243E] transition-all duration-300 cursor-pointer whitespace-nowrap inline-block" >Send Help {""}</button>
+                <button onClick={requestHelp} className={`${loading ? "bg-[#F2243E] text-white" : "bg-[#F6F6F6]"} py-[1.3rem] w-full text-[1.8rem] font-bold text-center rounded-lg hover:text-white hover:bg-[#F2243E] transition-all duration-300 cursor-pointer whitespace-nowrap inline-block`}>
+                  {loading ? "Sending..." : "Send Help"}
+                </button>
             </div>
           </motion.div>                                                                                                                                                                                         
         </motion.div>}
