a3fab08bf4ba62dfc94ade19288ac72d1eb99b50
Temidire <temidireowoeye@gmail.com>
2024-11-15 11:17:46 +0100

    fix: landing

--- src/pages/Landing.tsx
+++ src/pages/Landing.tsx
@@ -274,10 +274,10 @@ const Landing = () => {
                     </div>
 
                     <div className="relative h-[calc(100vh-(0.45*100vh))]">
-                        <motion.img src={'/assets/icons/Rectangle1.svg'} alt="frame 1" className="w-[7.2rem] lg:w-[12rem] xl:w-[20rem] absolute bottom-[500px] left-[50%] lg:bottom-[500px] lg:left-[80%] transform -translate-x-1/2 " animate={{ y: [0, -100, 0] }} transition={{ duration: 5, ease: "easeInOut", repeat: Infinity, repeatType: "mirror", }} />
-                        <motion.img src={'/assets/icons/Rectangle2.svg'} alt="frame 2" className="w-[12rem] lg:w-[20rem] absolute top-[50px] left-[40%] lg:top-[50px] transform -translate-x-1/2" animate={{ y: [0, -45, 100, 0] }} transition={{ duration: 7, ease: "easeInOut", repeat: Infinity, repeatType: "mirror", }} />
-                        <motion.img src={'/assets/icons/Rectangle3.svg'} alt="frame 3" className="w-[12rem] lg:w-[20rem] absolute top-[150px] left-[75%] lg:top-[150px] lg:left-[70%] transform -translate-x-1/2" animate={{ y: [0, -50, 100, 0] }} transition={{ duration: 8, ease: "easeInOut", repeat: Infinity, repeatType: "mirror", }} />
-                        <motion.img src={'/assets/icons/Rectangle4.svg'} alt="frame 4" className="w-[12rem] lg:w-[20rem] absolute bottom-[50px] left-[0%] lg:bottom-[20px] lg:left-[1%] transform" animate={{ y: [0, -200, 0] }} transition={{ duration: 5, ease: "easeInOut", repeat: Infinity, repeatType: "mirror", }} />
+                        <img src={'/assets/icons/Rectangle1.svg'} alt="frame 1" className="w-[7.2rem] lg:w-[12rem] xl:w-[20rem] absolute bottom-[500px] left-[50%] lg:bottom-[500px] lg:left-[80%] transform -translate-x-1/2 "  />
+                        <img src={'/assets/icons/Rectangle2.svg'} alt="frame 2" className="w-[12rem] lg:w-[20rem] absolute top-[50px] left-[40%] lg:top-[50px] transform -translate-x-1/2" />
+                        <img src={'/assets/icons/Rectangle3.svg'} alt="frame 3" className="w-[12rem] lg:w-[20rem] absolute top-[150px] left-[75%] lg:top-[150px] lg:left-[70%] transform -translate-x-1/2"/>
+                        <img src={'/assets/icons/Rectangle4.svg'} alt="frame 4" className="w-[12rem] lg:w-[20rem] absolute bottom-[50px] left-[0%] lg:bottom-[20px] lg:left-[1%] transform" />
                     </div>
                 </section>
 
